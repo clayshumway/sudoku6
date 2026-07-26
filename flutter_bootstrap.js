4d1ba3f6dfe27c130441b45e5c5220856dd371a6
Clay Shumway <clay.shumway@gmail.com>
2026-07-26 08:52:41 -0700

    Deploy web build

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"83675ed27633283e7fc296c8bca22e841224c0
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "571761635" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1257691664" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
